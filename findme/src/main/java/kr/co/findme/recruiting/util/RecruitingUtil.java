71b7f0737044cce9cf9bcd6c3218de7b099c3365
Jongmin <da6225@gmail.com>
2018-06-05 20:15:10 +0900

    RecrutingUtil class 생성

---
+++ findme/src/main/java/kr/co/findme/recruiting/util/RecruitingUtil.java
@@ -0,0 +1,16 @@
+package kr.co.findme.recruiting.util;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class RecruitingUtil {
+
+
+}
