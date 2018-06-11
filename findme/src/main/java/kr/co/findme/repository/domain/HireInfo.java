a8d8e02dde42f8495ef3523bce0c9e61e652a069
Jongmin <da6225@gmail.com>
2018-06-11 18:17:33 +0900

    HireInfo

---
+++ findme/src/main/java/kr/co/findme/repository/domain/HireInfo.java
@@ -0,0 +1,40 @@
+package kr.co.findme.repository.domain;
+
+public class HireInfo {
+	private String moditimes;
+	private String expitimes;
+	private String companies;
+	private String positions;
+	private int matchs;
+	public String getModitimes() {
+		return moditimes;
+	}
+	public void setModitimes(String moditimes) {
+		this.moditimes = moditimes;
+	}
+	public String getExpitimes() {
+		return expitimes;
+	}
+	public void setExpitimes(String expitimes) {
+		this.expitimes = expitimes;
+	}
+	public String getCompanies() {
+		return companies;
+	}
+	public void setCompanies(String companies) {
+		this.companies = companies;
+	}
+	public String getPositions() {
+		return positions;
+	}
+	public void setPositions(String positions) {
+		this.positions = positions;
+	}
+	public int getMatchs() {
+		return matchs;
+	}
+	public void setMatchs(int matchs) {
+		this.matchs = matchs;
+	}
+	
+}
